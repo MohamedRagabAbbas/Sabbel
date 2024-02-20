587699f35607ebbdfede1c7d34294e14bbf59e1b
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-20 16:07:33 +0200

    add some elements to the html and css files for navbar and home pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './Pages/navbar/navbar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    NavbarComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

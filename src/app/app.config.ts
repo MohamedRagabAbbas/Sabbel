85202571e6f323742b00677eba0459c42222dee4
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-27 20:25:46 +0200

    add interceptor to send the jwt token with all the requests that require authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, withInterceptors } from '@angular/common/http';
 import { AuthenticationService } from './Services/authentication.service';
-
-
+import { provideHttpClient } from '@angular/common/http';
+import { authInterceptorInterceptor } from './Services/auth-interceptor.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     HttpClientModule, 
-    AuthenticationService],
+    AuthenticationService,
+    provideHttpClient(withInterceptors([authInterceptorInterceptor]))
+  ]
 };

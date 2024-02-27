e0e08e153a42d88d68fe1622b6e7a7bd3d673823
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-27 20:35:46 +0200

    add new model: Team

---
+++ src/app/Models/team-member/team-memebr.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeamMemberModule {
+  id:number = 0;
+  name:string = "";
+  position:string = "";
+  description:string = "";
+  image:string = "";
+ }

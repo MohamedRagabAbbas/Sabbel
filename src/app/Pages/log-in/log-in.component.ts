7642e4a89088e85798be7d46f06b5b549eeff32d
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-27 15:33:07 +0200

    add form group in the login component plus adding an authentication service

--- src/app/Pages/log-in/log-in.component.ts
+++ src/app/Pages/log-in/log-in.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'log-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
 export class LogInComponent {
-  constructor(private router:Router)
+  formGroup:FormGroup = new FormGroup({});
+  constructor(private router:Router, private formBuilder:FormBuilder)
   {
-
+    this.formGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$')]]
+    });
   }
   LogIn()
   {
     console.log("Log In");
-    this.router.navigate(['/dashboard']);
+    console.log(this.formGroup.value);
+    console.log(this.formGroup.controls['email'].value);
+    console.log(this.formGroup.controls['password'].value);
+    //this.router.navigate(['/dashboard']);
   }
 }

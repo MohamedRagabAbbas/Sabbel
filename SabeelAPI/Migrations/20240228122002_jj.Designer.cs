54957520d2ef2795e8cfa40afa02339c4526f4ce
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-28 14:21:20 +0200

    found an issue in the relation between image and event and fixed it

--- SabeelAPI/Migrations/20240228122002_jj.Designer.cs
+++ SabeelAPI/Migrations/20240228122002_jj.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SabeelAPI.DB;
 
@@ -11,9 +12,11 @@ using SabeelAPI.DB;
 namespace SabeelAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240228122002_jj")]
+    partial class jj
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SabeelAPI/Migrations/20240228122002_jj.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SabeelAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class jj : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ImageId",
+                table: "Events");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageId",
+                table: "Events",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ImageId",
+                table: "Events",
+                column: "ImageId",
+                unique: true,
+                filter: "[ImageId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ImageId",
+                table: "Events");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageId",
+                table: "Events",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageId",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageId",
+                value: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ImageId",
+                table: "Events",
+                column: "ImageId",
+                unique: true);
+        }
+    }
+}

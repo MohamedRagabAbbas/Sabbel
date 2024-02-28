376df935c821c72795fc2653b1e4c9a84dd9d49d
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-28 14:23:13 +0200

    the issue is stil exist, so trying another solution

--- SabeelAPI/Models/Image.cs
+++ SabeelAPI/Models/Image.cs
@@ -6,7 +6,9 @@ namespace SabeelAPI.Models
     public byte[]? ImageData { get; set; }
     public string ImageDescription { get; set; } = string.Empty;
 
+    public int EventId { get; set; }
     public Event? Event { get; set; }
+    public int TeemMemberId { get; set; }
     public TeemMember? TeemMember { get; set; }
   }
 }
--- SabeelAPI/Models/TeemMember.cs
+++ SabeelAPI/Models/TeemMember.cs
@@ -6,7 +6,6 @@ namespace SabeelAPI.Models
     public string Name { get; set; } = string.Empty;
     public string Postion = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public int ImageId { get; set; }
     public Image? Image { get; set; }
   }
 }

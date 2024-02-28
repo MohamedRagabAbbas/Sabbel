dbd715c02fdad10b34fd14a6e509505f7979b8f3
MohamedRagabAbbas <Mohamed_Ragab02@aucegypt.edu>
2024-02-28 16:23:23 +0200

    Add Image Attributes

--- src/app/Models/image/image.module.ts
+++ src/app/Models/image/image.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EventModule } from '../event/event.module';
+import { TeamMemberModule } from '../team-member/team-memebr.module';
 
 
 
@@ -9,4 +11,12 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class ImageModule { }
+export class ImageModule {
+  id:number = 0;
+  imageData:File = new File([""], "filename");
+  imageDescription:string = "";
+  eventId:number = 0;
+  event:EventModule = new EventModule();
+  teamMemberId:number = 0;
+  teamMember:TeamMemberModule = new TeamMemberModule();
+ }
